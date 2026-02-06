df8ec16edf400c633fc546ff6c5e86d1e62bb1e4
Sina Jokar <cnajokar11@yahoo.com>
2026-02-06 13:22:01 +0300

    Correctly fixing static paths

--- core/settings.py
+++ core/settings.py
@@ -93,7 +93,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "core" / "static",
 ]
 
 MEDIA_URL = '/media/'
