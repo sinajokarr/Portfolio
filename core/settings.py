bd1c68fa85ccd7807215d45828c994de6e661ca7
Sina Jokar <cnajokar11@yahoo.com>
2026-02-06 12:48:57 +0300

    Switch to SQLite and remove mysqlclient for Vercel

--- core/settings.py
+++ core/settings.py
@@ -59,12 +59,7 @@ TEMPLATES = [{
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 AUTH_PASSWORD_VALIDATORS = [
     { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
